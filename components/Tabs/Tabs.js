abd2ea92eb4fd329a0060f9c34e11ac320cb6437
yunlu7 <2733734845@qq.com>
2022-11-19 22:24:58 +0800

    实现搜索框和tabs组件

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,7 +4,10 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    tabs:{
+      type:Array,
+      value:[]
+    }
   },
 
   /**
@@ -18,6 +21,12 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    // 点击事件
+    handleItemTap(e){
+      // 1 获取点击的索引
+      const {index}=e.currentTarget.dataset;
+      // 2 触发 父组件中的事件 自定义
+      this.triggerEvent("tabsItemChange",{index});
+    }
   }
 })
