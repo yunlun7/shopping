a1920b0341d1c478080951ee0ab14a13347487a2
yunlu7 <2733734845@qq.com>
2022-11-21 19:55:01 +0800

    解决index页面样式问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,55 @@
-// 引入用来发送请求的方法
-import{request} from "../../request/index.js";
-
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// 0 引入 用来发送请求的 方法 一定要把路径补全
+import { request } from "../../request/index.js";
 Page({
   data: {
-    // 轮播图数组
-    swiperList:[],
+    //轮播图数组
+    swiperList: [],
     // 导航 数组
-    catesList:[],
-    // 楼层数组
-    floorList:[]
+    catesList: [],
+    // 楼层数据
+    floorList: []
   },
-
-  onLoad() {
-
+  //页面开始加载就会触发
+  onLoad: function (options) {
     // 1 发送异步请求获取轮播图数据  优化的手段可以通过es6的 promise来解决这个问题 
-/*      wx.request({
-       url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-       success: (result) => {
-        // console.log(result);
-          this.setData({
-           swiperList: result.data.message
-          
-         }) 
-       }
-     }); */
-
+    //   wx.request({
+    //     url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //     success: (result) => {
+    //       this.setData({
+    //         swiperList: result.data.message
+    //       })
+    //       // console.log(result);
+    //     }
+    //   });
     this.getSwiperList();
     this.getCateList();
     this.getFloorList();
   },
-  
   // 获取轮播图数据
-  getSwiperList(){
-    request({url:"/home/swiperdata"})
-    .then(result => {
-      this.setData({
-        swiperList: result
+  getSwiperList() {
+    request({ url: "/home/swiperdata" })
+      .then(result => {
+        this.setData({
+          swiperList: result
+        })
       })
-    })
   },
-
-  // 获取分类导航数据
-  getCateList(){
+  // 获取 分类导航数据
+  getCateList() {
     request({ url: "/home/catitems" })
-    .then(result => {
-      this.setData({
-        catesList: result
+      .then(result => {
+        this.setData({
+          catesList: result
+        })
       })
-    })
   },
-
-  // 获取楼层数据
-  getFloorList(){
+  // 获取 楼层数据
+  getFloorList() {
     request({ url: "/home/floordata" })
-    .then(result => {
-      this.setData({
-        floorList: result
+      .then(result => {
+        this.setData({
+          floorList: result
+        })
       })
-    })
-  } 
-})
+  },
+});
