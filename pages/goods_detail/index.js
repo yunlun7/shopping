bce3565d18c77c58c30bfddbcd076e082536079b
yunlu7 <2733734845@qq.com>
2022-11-21 22:31:45 +0800

    商品详情-获取数据

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,38 @@
 // pages/goods_detail/index.js
+/* 
+1、发送请求，获取信息
+*/
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const {goods_id} = options;
+    console.log(goods_id);
+    this.getGoodsDetail(goods_id)
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  // 获取商品详情数据
+  async getGoodsDetail(goods_id) {
+    const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
+    this.setData({
+      goodsObj
+    })
 
   }
+
+ 
 })
