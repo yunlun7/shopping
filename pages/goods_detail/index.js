edbff779e672bacbdc7d75b31324138e6a757595
yunlu7 <2733734845@qq.com>
2022-11-21 23:23:25 +0800

    放大预览图片

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,6 +1,9 @@
 // pages/goods_detail/index.js
 /* 
 1、发送请求，获取信息
+2、点击轮播图 预览大图
+  1、给轮播图绑定点击事件
+  2、调用小程序的api previewImage
 */
 import { request } from "../../request/index.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
@@ -14,13 +17,15 @@ Page({
   data: {
     goodsObj:{}
   },
+  // 商品对象
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     const {goods_id} = options;
-    console.log(goods_id);
+    // console.log(goods_id);
     this.getGoodsDetail(goods_id)
 
   },
@@ -28,6 +33,7 @@ Page({
   // 获取商品详情数据
   async getGoodsDetail(goods_id) {
     const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
+    this.GoodsInfo = goodsObj;
     this.setData({
       // 优化：将需要用到的数据领出来，其余不需要的就舍弃
       goodsObj:{
@@ -41,6 +47,17 @@ Page({
       }
     })
 
+  },
+  // 点击轮播图  放大预览
+  handlePreviewImage(e){
+    // 1 先构造要预览的图片数组 
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
+    // 2 接收传递过来的图片url
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+    });
   }
 
  
