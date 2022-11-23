2412919cbe39bb740a29c9a9b899eafae20ef208
yunlu7 <2733734845@qq.com>
2022-11-23 17:02:19 +0800

    商品详情完结

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,9 +1,8 @@
-// pages/goods_detail/index.js
 /* 
-1、发送请求，获取信息
-2、点击轮播图 预览大图
-  1、给轮播图绑定点击事件
-  2、调用小程序的api previewImage
+1 发送请求获取数据 
+2 点击轮播图 预览大图
+  1 给轮播图绑定点击事件
+  2 调用小程序的api  previewImage 
 3 点击 加入购物车
   1 先绑定点击事件
   2 获取缓存中的购物车数据 数组格式 
@@ -20,39 +19,41 @@
     1 判断该商品是否存在于缓存数组中
     2 已经存在 把该商品删除
     3 没有存在 把商品添加到收藏数组中 存入到缓存中即可
-*/
+ */
 import { request } from "../../request/index.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
-
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    goodsObj:{}
+    goodsObj: {},
+    // 商品是否被收藏
+    isCollect:false
   },
   // 商品对象
   GoodsInfo: {},
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    const {goods_id} = options;
-    // console.log(goods_id);
-    this.getGoodsDetail(goods_id)
-
+  onShow: function () {
+    let pages = getCurrentPages();
+    let currentPage = pages[pages.length - 1];
+    let options = currentPage.options;
+    const { goods_id } = options;
+    this.getGoodsDetail(goods_id);
   },
-
   // 获取商品详情数据
   async getGoodsDetail(goods_id) {
     const goodsObj = await request({ url: "/goods/detail", data: { goods_id } });
     this.GoodsInfo = goodsObj;
+    // 1 获取缓存中的商品收藏的数组
+    let collect = wx.getStorageSync("collect") || [];
+    // 2 判断当前商品是否被收藏
+    let isCollect = collect.some(v => v.goods_id === this.GoodsInfo.goods_id);
     this.setData({
-      // 优化：将需要用到的数据领出来，其余不需要的就舍弃
-      goodsObj:{
+      goodsObj: {
         goods_name: goodsObj.goods_name,
         goods_price: goodsObj.goods_price,
         // iphone部分手机 不识别 webp图片格式 
@@ -60,12 +61,12 @@ Page({
         // 临时自己改 确保后台存在 1.webp => 1.jpg 
         goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
         pics: goodsObj.pics
-      }
+      },
+      isCollect
     })
-
   },
-  // 点击轮播图  放大预览
-  handlePreviewImage(e){
+  // 点击轮播图 放大预览
+  handlePrevewImage(e) {
     // 1 先构造要预览的图片数组 
     const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
     // 2 接收传递过来的图片url
@@ -74,9 +75,10 @@ Page({
       current,
       urls
     });
+
   },
-  // 点击加入购物车
-  handleCartAdd(){
+  // 点击 加入购物车
+  handleCartAdd() {
     // 1 获取缓存中的购物车 数组
     let cart = wx.getStorageSync("cart") || [];
     // 2 判断 商品对象是否存在于购物车数组中
@@ -84,7 +86,6 @@ Page({
     if (index === -1) {
       //3  不存在 第一次添加
       this.GoodsInfo.num = 1;
-      // 设置选中的状态  初始值为true
       this.GoodsInfo.checked = true;
       cart.push(this.GoodsInfo);
     } else {
@@ -100,7 +101,46 @@ Page({
       // true 防止用户 手抖 疯狂点击按钮 
       mask: true
     });
+
+
+
+  },
+  // 点击 商品收藏图标
+  handleCollect(){
+    let isCollect=false;
+    // 1 获取缓存中的商品收藏数组
+    let collect=wx.getStorageSync("collect")||[];
+    // 2 判断该商品是否被收藏过
+    let index=collect.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
+    // 3 当index！=-1表示 已经收藏过 
+    if(index!==-1){
+      // 能找到 已经收藏过了  在数组中删除该商品
+      collect.splice(index,1);
+      isCollect=false;
+      wx.showToast({
+        title: '取消成功',
+        icon: 'success',
+        mask: true
+      });
+        
+    }else{
+      // 没有收藏过
+      collect.push(this.GoodsInfo);
+      isCollect=true;
+      wx.showToast({
+        title: '收藏成功',
+        icon: 'success',
+        mask: true
+      });
+    }
+    // 4 把数组存入到缓存中
+    wx.setStorageSync("collect", collect);
+    // 5 修改data中的属性  isCollect
+    this.setData({
+      isCollect
+    })
+      
+      
   }
 
- 
 })
